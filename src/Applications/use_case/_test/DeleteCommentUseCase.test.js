0371d2ae9a1add06e85f6ec3254c142f3a8bdf1b
veda <vedabezaleel@gmail.com>
2026-05-11 22:18:00 +0700

    test: DeleteCommentUseCase — include 403 and 404 cases

---
+++ src/Applications/use_case/_test/DeleteCommentUseCase.test.js
@@ -0,0 +1,43 @@
+import CommentRepository from '../../../Domains/comments/CommentRepository';
+import ThreadRepository from '../../../Domains/threads/ThreadRepository';
+import DeleteCommentUseCase from '../DeleteCommentUseCase';
+
+describe('DeleteCommentUseCase', () => {
+  it('should orchestrating the delete comment action correctly', async () => {
+    // Arrange
+    const useCasePayload = {
+      threadId: 'thread-123',
+      commentId: 'comment-123',
+      owner: 'user-123',
+    };
+
+    /** creating dependency of use case */
+    const mockCommentRepository = new CommentRepository();
+    const mockThreadRepository = new ThreadRepository();
+
+    /** mocking needed function */
+    mockThreadRepository.verifyThreadExists = vi.fn()
+      .mockImplementation(() => Promise.resolve());
+    mockCommentRepository.verifyCommentExists = vi.fn()
+      .mockImplementation(() => Promise.resolve());
+    mockCommentRepository.verifyCommentOwner = vi.fn()
+      .mockImplementation(() => Promise.resolve());
+    mockCommentRepository.deleteComment = vi.fn()
+      .mockImplementation(() => Promise.resolve());
+
+    /** creating use case instance */
+    const deleteCommentUseCase = new DeleteCommentUseCase({
+      commentRepository: mockCommentRepository,
+      threadRepository: mockThreadRepository,
+    });
+
+    // Action
+    await deleteCommentUseCase.execute(useCasePayload);
+
+    // Assert
+    expect(mockThreadRepository.verifyThreadExists).toBeCalledWith(useCasePayload.threadId);
+    expect(mockCommentRepository.verifyCommentExists).toBeCalledWith(useCasePayload.commentId);
+    expect(mockCommentRepository.verifyCommentOwner).toBeCalledWith(useCasePayload.commentId, useCasePayload.owner);
+    expect(mockCommentRepository.deleteComment).toBeCalledWith(useCasePayload.commentId);
+  });
+});
