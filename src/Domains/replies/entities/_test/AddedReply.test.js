c854740f6327b0e3a0f030fe7c234c5dbc74520e
veda <vedabezaleel@gmail.com>
2026-05-11 20:12:00 +0700

    feat: add NewReply entity with payload validation

---
+++ src/Domains/replies/entities/_test/AddedReply.test.js
@@ -0,0 +1,43 @@
+import AddedReply from '../AddedReply';
+
+describe('a AddedReply entities', () => {
+  it('should throw error when payload not contain needed property', () => {
+    // Arrange
+    const payload = {
+      content: 'abc',
+      owner: 'user-123',
+    };
+
+    // Action and Assert
+    expect(() => new AddedReply(payload)).toThrowError('ADDED_REPLY.NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
+  it('should throw error when payload not meet data type specification', () => {
+    // Arrange
+    const payload = {
+      id: 123,
+      content: 'abc',
+      owner: 'user-123',
+    };
+
+    // Action and Assert
+    expect(() => new AddedReply(payload)).toThrowError('ADDED_REPLY.NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
+
+  it('should create AddedReply object correctly', () => {
+    // Arrange
+    const payload = {
+      id: 'reply-123',
+      content: 'abc',
+      owner: 'user-123',
+    };
+
+    // Action
+    const { id, content, owner } = new AddedReply(payload);
+
+    // Assert
+    expect(id).toEqual(payload.id);
+    expect(content).toEqual(payload.content);
+    expect(owner).toEqual(payload.owner);
+  });
+});
