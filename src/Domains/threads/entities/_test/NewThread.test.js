449cdd28d32b45478a5b7f26e2221c38dbb54f1e
veda <vedabezaleel@gmail.com>
2026-05-11 18:48:00 +0700

    test: NewThread entity — expect validation errors

---
+++ src/Domains/threads/entities/_test/NewThread.test.js
@@ -0,0 +1,43 @@
+import NewThread from '../NewThread';
+
+describe('a NewThread entities', () => {
+  it('should throw error when payload not contain needed property', () => {
+    // Arrange
+    const payload = {
+      title: 'abc',
+      body: 'abc',
+    };
+
+    // Action and Assert
+    expect(() => new NewThread(payload)).toThrowError('NEW_THREAD.NOT_CONTAIN_NEEDED_PROPERTY');
+  });
+
+  it('should throw error when payload not meet data type specification', () => {
+    // Arrange
+    const payload = {
+      title: 123,
+      body: 'abc',
+      owner: 'user-123',
+    };
+
+    // Action and Assert
+    expect(() => new NewThread(payload)).toThrowError('NEW_THREAD.NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
+
+  it('should create NewThread object correctly', () => {
+    // Arrange
+    const payload = {
+      title: 'abc',
+      body: 'abc',
+      owner: 'user-123',
+    };
+
+    // Action
+    const { title, body, owner } = new NewThread(payload);
+
+    // Assert
+    expect(title).toEqual(payload.title);
+    expect(body).toEqual(payload.body);
+    expect(owner).toEqual(payload.owner);
+  });
+});
