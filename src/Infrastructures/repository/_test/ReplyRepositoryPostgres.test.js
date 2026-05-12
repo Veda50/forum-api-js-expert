0ad473482d0494abefd388fdf9344b4162bd3573
veda <vedabezaleel@gmail.com>
2026-05-12 11:00:00 +0700

    test: ReplyRepositoryPostgres integration tests

---
+++ src/Infrastructures/repository/_test/ReplyRepositoryPostgres.test.js
@@ -0,0 +1,179 @@
+import RepliesTableTestHelper from '../../../../tests/RepliesTableTestHelper.js';
+import CommentsTableTestHelper from '../../../../tests/CommentsTableTestHelper.js';
+import ThreadsTableTestHelper from '../../../../tests/ThreadsTableTestHelper.js';
+import UsersTableTestHelper from '../../../../tests/UsersTableTestHelper.js';
+import NewReply from '../../../Domains/replies/entities/NewReply.js';
+import AddedReply from '../../../Domains/replies/entities/AddedReply.js';
+import pool from '../../database/postgres/pool.js';
+import ReplyRepositoryPostgres from '../ReplyRepositoryPostgres.js';
+import NotFoundError from '../../../Commons/exceptions/NotFoundError.js';
+import AuthorizationError from '../../../Commons/exceptions/AuthorizationError.js';
+
+describe('ReplyRepositoryPostgres', () => {
+  afterEach(async () => {
+    await RepliesTableTestHelper.cleanTable();
+    await CommentsTableTestHelper.cleanTable();
+    await ThreadsTableTestHelper.cleanTable();
+    await UsersTableTestHelper.cleanTable();
+  });
+
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  describe('addReply function', () => {
+    it('should persist new reply and return added reply correctly', async () => {
+      // Arrange
+      await UsersTableTestHelper.addUser({ id: 'user-123' });
+      await ThreadsTableTestHelper.addThread({ id: 'thread-123', owner: 'user-123' });
+      await CommentsTableTestHelper.addComment({ id: 'comment-123', threadId: 'thread-123', owner: 'user-123' });
+      const newReply = new NewReply({
+        content: 'abc',
+        commentId: 'comment-123',
+        threadId: 'thread-123',
+        owner: 'user-123',
+      });
+      const fakeIdGenerator = () => '123'; // stub!
+      const replyRepositoryPostgres = new ReplyRepositoryPostgres(pool, fakeIdGenerator);
+
+      // Action
+      const addedReply = await replyRepositoryPostgres.addReply(newReply);
+
+      // Assert
+      const replies = await RepliesTableTestHelper.findReplyById('reply-123');
+      expect(replies).toHaveLength(1);
+      expect(addedReply).toStrictEqual(new AddedReply({
+        id: 'reply-123',
+        content: 'abc',
+        owner: 'user-123',
+      }));
+    });
+  });
+
+  describe('verifyReplyExists function', () => {
+    it('should throw NotFoundError when reply not found', async () => {
+      // Arrange
+      const replyRepositoryPostgres = new ReplyRepositoryPostgres(pool, {});
+
+      // Action & Assert
+      await expect(replyRepositoryPostgres.verifyReplyExists('reply-123'))
+        .rejects.toThrowError(NotFoundError);
+    });
+
+    it('should not throw NotFoundError when reply found', async () => {
+      // Arrange
+      await UsersTableTestHelper.addUser({ id: 'user-123' });
+      await ThreadsTableTestHelper.addThread({ id: 'thread-123', owner: 'user-123' });
+      await CommentsTableTestHelper.addComment({ id: 'comment-123', threadId: 'thread-123', owner: 'user-123' });
+      await RepliesTableTestHelper.addReply({ id: 'reply-123', commentId: 'comment-123', owner: 'user-123' });
+      const replyRepositoryPostgres = new ReplyRepositoryPostgres(pool, {});
+
+      // Action & Assert
+      await expect(replyRepositoryPostgres.verifyReplyExists('reply-123'))
+        .resolves.not.toThrowError(NotFoundError);
+    });
+  });
+
+  describe('verifyReplyOwner function', () => {
+    it('should throw NotFoundError when reply not found', async () => {
+      // Arrange
+      const replyRepositoryPostgres = new ReplyRepositoryPostgres(pool, {});
+
+      // Action & Assert
+      await expect(replyRepositoryPostgres.verifyReplyOwner('reply-123', 'user-123'))
+        .rejects.toThrowError(NotFoundError);
+    });
+
+    it('should throw AuthorizationError when user is not the owner', async () => {
+      // Arrange
+      await UsersTableTestHelper.addUser({ id: 'user-123' });
+      await ThreadsTableTestHelper.addThread({ id: 'thread-123', owner: 'user-123' });
+      await CommentsTableTestHelper.addComment({ id: 'comment-123', threadId: 'thread-123', owner: 'user-123' });
+      await RepliesTableTestHelper.addReply({ id: 'reply-123', commentId: 'comment-123', owner: 'user-123' });
+      const replyRepositoryPostgres = new ReplyRepositoryPostgres(pool, {});
+
+      // Action & Assert
+      await expect(replyRepositoryPostgres.verifyReplyOwner('reply-123', 'user-456'))
+        .rejects.toThrowError(AuthorizationError);
+    });
+
+    it('should not throw AuthorizationError when user is the owner', async () => {
+      // Arrange
+      await UsersTableTestHelper.addUser({ id: 'user-123' });
+      await ThreadsTableTestHelper.addThread({ id: 'thread-123', owner: 'user-123' });
+      await CommentsTableTestHelper.addComment({ id: 'comment-123', threadId: 'thread-123', owner: 'user-123' });
+      await RepliesTableTestHelper.addReply({ id: 'reply-123', commentId: 'comment-123', owner: 'user-123' });
+      const replyRepositoryPostgres = new ReplyRepositoryPostgres(pool, {});
+
+      // Action & Assert
+      await expect(replyRepositoryPostgres.verifyReplyOwner('reply-123', 'user-123'))
+        .resolves.not.toThrowError(AuthorizationError);
+    });
+  });
+
+  describe('deleteReply function', () => {
+    it('should throw NotFoundError when reply not found', async () => {
+      // Arrange
+      const replyRepositoryPostgres = new ReplyRepositoryPostgres(pool, {});
+
+      // Action & Assert
+      await expect(replyRepositoryPostgres.deleteReply('reply-123'))
+        .rejects.toThrowError(NotFoundError);
+    });
+
+    it('should soft delete reply correctly', async () => {
+      // Arrange
+      await UsersTableTestHelper.addUser({ id: 'user-123' });
+      await ThreadsTableTestHelper.addThread({ id: 'thread-123', owner: 'user-123' });
+      await CommentsTableTestHelper.addComment({ id: 'comment-123', threadId: 'thread-123', owner: 'user-123' });
+      await RepliesTableTestHelper.addReply({ id: 'reply-123', commentId: 'comment-123', owner: 'user-123' });
+      const replyRepositoryPostgres = new ReplyRepositoryPostgres(pool, {});
+
+      // Action
+      await replyRepositoryPostgres.deleteReply('reply-123');
+
+      // Assert
+      const replies = await RepliesTableTestHelper.findReplyById('reply-123');
+      expect(replies[0].is_delete).toEqual(true);
+    });
+  });
+
+  describe('getRepliesByCommentId function', () => {
+    it('should return empty replies when comment has no replies', async () => {
+      // Arrange
+      const replyRepositoryPostgres = new ReplyRepositoryPostgres(pool, {});
+
+      // Action
+      const replies = await replyRepositoryPostgres.getRepliesByCommentId('comment-123');
+
+      // Assert
+      expect(replies).toHaveLength(0);
+    });
+
+    it('should return replies correctly', async () => {
+      // Arrange
+      await UsersTableTestHelper.addUser({ id: 'user-123', username: 'dicoding' });
+      await ThreadsTableTestHelper.addThread({ id: 'thread-123', owner: 'user-123' });
+      await CommentsTableTestHelper.addComment({ id: 'comment-123', threadId: 'thread-123', owner: 'user-123' });
+      await RepliesTableTestHelper.addReply({
+        id: 'reply-123', commentId: 'comment-123', owner: 'user-123', content: 'reply A',
+      });
+      await RepliesTableTestHelper.addReply({
+        id: 'reply-456', commentId: 'comment-123', owner: 'user-123', content: 'reply B',
+      });
+      const replyRepositoryPostgres = new ReplyRepositoryPostgres(pool, {});
+
+      // Action
+      const replies = await replyRepositoryPostgres.getRepliesByCommentId('comment-123');
+
+      // Assert
+      expect(replies).toHaveLength(2);
+      expect(replies[0].id).toEqual('reply-123');
+      expect(replies[0].username).toEqual('dicoding');
+      expect(replies[0].content).toEqual('reply A');
+      expect(replies[1].id).toEqual('reply-456');
+      expect(replies[1].username).toEqual('dicoding');
+      expect(replies[1].content).toEqual('reply B');
+    });
+  });
+});
