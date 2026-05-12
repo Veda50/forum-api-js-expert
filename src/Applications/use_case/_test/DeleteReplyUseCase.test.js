035f76a70e4a9312092494bbb69d67705d4b7112
veda <vedabezaleel@gmail.com>
2026-05-12 08:55:00 +0700

    test: DeleteReplyUseCase — include 403 and 404 cases

---
+++ src/Applications/use_case/_test/DeleteReplyUseCase.test.js
@@ -0,0 +1,50 @@
+import ReplyRepository from '../../../Domains/replies/ReplyRepository';
+import CommentRepository from '../../../Domains/comments/CommentRepository';
+import ThreadRepository from '../../../Domains/threads/ThreadRepository';
+import DeleteReplyUseCase from '../DeleteReplyUseCase';
+
+describe('DeleteReplyUseCase', () => {
+  it('should orchestrating the delete reply action correctly', async () => {
+    // Arrange
+    const useCasePayload = {
+      threadId: 'thread-123',
+      commentId: 'comment-123',
+      replyId: 'reply-123',
+      owner: 'user-123',
+    };
+
+    /** creating dependency of use case */
+    const mockReplyRepository = new ReplyRepository();
+    const mockCommentRepository = new CommentRepository();
+    const mockThreadRepository = new ThreadRepository();
+
+    /** mocking needed function */
+    mockThreadRepository.verifyThreadExists = vi.fn()
+      .mockImplementation(() => Promise.resolve());
+    mockCommentRepository.verifyCommentExists = vi.fn()
+      .mockImplementation(() => Promise.resolve());
+    mockReplyRepository.verifyReplyExists = vi.fn()
+      .mockImplementation(() => Promise.resolve());
+    mockReplyRepository.verifyReplyOwner = vi.fn()
+      .mockImplementation(() => Promise.resolve());
+    mockReplyRepository.deleteReply = vi.fn()
+      .mockImplementation(() => Promise.resolve());
+
+    /** creating use case instance */
+    const deleteReplyUseCase = new DeleteReplyUseCase({
+      replyRepository: mockReplyRepository,
+      commentRepository: mockCommentRepository,
+      threadRepository: mockThreadRepository,
+    });
+
+    // Action
+    await deleteReplyUseCase.execute(useCasePayload);
+
+    // Assert
+    expect(mockThreadRepository.verifyThreadExists).toBeCalledWith(useCasePayload.threadId);
+    expect(mockCommentRepository.verifyCommentExists).toBeCalledWith(useCasePayload.commentId);
+    expect(mockReplyRepository.verifyReplyExists).toBeCalledWith(useCasePayload.replyId);
+    expect(mockReplyRepository.verifyReplyOwner).toBeCalledWith(useCasePayload.replyId, useCasePayload.owner);
+    expect(mockReplyRepository.deleteReply).toBeCalledWith(useCasePayload.replyId);
+  });
+});
