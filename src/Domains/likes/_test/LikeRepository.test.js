3d1096fa9d17e386da2ef330168143e6573d1c20
veda <vedabezaleel@gmail.com>
2026-05-17 23:33:35 +0700

    test: simulate failing test case for CI scenario

--- src/Domains/likes/_test/LikeRepository.test.js
+++ src/Domains/likes/_test/LikeRepository.test.js
@@ -4,7 +4,7 @@ describe('LikeRepository interface', () => {
   it('should throw error when invoke abstract behavior', async () => {
     const likeRepository = new LikeRepository();
 
-    await expect(likeRepository.isCommentLikedByUser({})).rejects.toThrowError('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+    await expect(likeRepository.isCommentLikedByUser({})).resolves.toBe(true);
     await expect(likeRepository.likeComment({})).rejects.toThrowError('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
     await expect(likeRepository.unlikeComment({})).rejects.toThrowError('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
     await expect(likeRepository.getLikeCountByCommentId('')).rejects.toThrowError('LIKE_REPOSITORY.METHOD_NOT_IMPLEMENTED');
